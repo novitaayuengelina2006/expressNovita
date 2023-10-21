295b6f4580a86622510632eb1331bbfadd5a8afa
novitaayuengelina2006 <148603880+novitaayuengelina2006@users.noreply.github.com>
2023-10-21 10:34:05 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//import express
+import express from "express";
+
+//import router
+import Router  from "./routes/routes.js";
+
+// init express
+const app = express();
+
+//use Router
+app.use(Router);
+
+
+
+// listen on port
+app.listen(3000, () => console.log('server runing at http://localhost:3000'));
+
